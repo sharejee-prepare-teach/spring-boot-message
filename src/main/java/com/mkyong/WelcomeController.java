fba1de46fb399b40077699bd1f580e1da3636d6d
Rith <rithronlkh@gmail.com>
2018-08-16 21:41:27 +0700

    add oauth2

--- src/main/java/com/mkyong/WelcomeController.java
+++ src/main/java/com/mkyong/WelcomeController.java
@@ -13,7 +13,7 @@ public class WelcomeController {
 	@Value("${welcome.message:test}")
 	private String message = "Hello World";
 
-	@RequestMapping("/")
+	@RequestMapping("/welcome")
 	public String welcome(Map<String, Object> model) {
 		model.put("message", this.message);
 		return "welcome";
